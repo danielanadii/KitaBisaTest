4cbbbdf8f3d04df9416a72b7611f584493f202f2
DanielAnadi <danielanadii@gmail.com>
2020-12-12 00:31:19 +0700

    Initial Commit

---
+++ KitaBisa TechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KitaBisa TechTest
+//
+//  Created by Daniel Anadi on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
